d4ff3f2424df05c5f00b36802f06786f6b07735a
ROGER <ROGER@1064878O046.unah.edu>
2026-04-15 14:34:27 -0600

    fix: Se han agregaod nuevos proceimidnetos almacenados en el esquema Users

--- storageProcedures/users/splUsers.sql
+++ storageProcedures/users/splUsers.sql
@@ -1,6 +1,8 @@
 USE BdTest;
 GO
 
+---------------------------------------------------
+
 DROP PROCEDURE IF EXISTS [Users].splInsertUser;
 GO
 
@@ -20,24 +22,51 @@ AS
 BEGIN
   SET NOCOUNT OFF;
 
-  IF EXISTS (SELECT 1 FROM Users.tblUsers WHERE email = @email)
+  -- 1. Validar si el usuario existe
+  IF EXISTS (SELECT 1 FROM [Users].tblUsers WHERE email = @email)
   BEGIN 
-    RETURN -1
+    SELECT 
+      -1 AS code,
+      'El usuario con este correo ya existe' AS message;
+    RETURN;
   END
 
+  -- 2. Validar si un usuario ya posee esta contraseña
+  IF EXISTS (SELECT 1 FROM [Users].tblUsers WHERE [password] = @password)
+    BEGIN 
+    SELECT 
+      -2 AS code,
+      'Ya existe un usuario con esta contraseña' AS message;
+    RETURN;
+  END
+
+  -- 2. Inserta al nuevo usuario
   INSERT INTO [Users].tblUsers (fullname, idRole, email, password)
-  OUTPUT INSERTED.*
+  OUTPUT 
+    1 AS Code,
+    INSERTED.idUser,
+    INSERTED.idRole,
+    INSERTED.fullName,
+    INSERTED.email
   VALUES (@fullName, @idRole, @email, @password)
 END;
 GO
 
-EXEC [Users].splInsertUser @fullName = 'Royer B', @idRole = 1, @email = 'royer.b@mail.com', @password = 'passR';
+EXEC [Users].splInsertUser @fullName = 'Miguel Trejo', @idRole = 2, @email = 'miguel.trejo@mail.com', @password = 'passMi';
 GO
 
+SELECT * FROM [Users].tblUsers;
+
 -------------------------------------------------------------------
+
 DROP PROCEDURE IF EXISTS [Users].splUpdateUserPassword;
 GO
 
+/*
+  Procedimiento almacenado que actualiza la contraseña de un usuario existente y retorna la información del usuario que se ha modificado
+  @param email: Recibe el correo electrónico del usuario al que cambiará la contraseña
+  @para newPassword: Recibe la nueva contraseña que tendrá el usuario
+*/
 CREATE PROCEDURE [Users].splUpdateUserPassword
   @email NVARCHAR(255),
   @newPassword NVARCHAR(255)
@@ -45,16 +74,82 @@ AS
 BEGIN
   SET NOCOUNT ON;
 
+  -- 1. Validar si el usuario existe
   IF NOT EXISTS (SELECT 1 FROM [Users].tblUsers WHERE email = @email)
   BEGIN
-    RETURN -1;
+    SELECT 
+      -1 AS code,
+      'El correo no existe' AS message;
+    RETURN;
   END
 
-  UPDATE [Users].tblUsers SET password = @newPassword WHERE email = @email;
+  -- 2. Validar si la contraseña es la misma
+  IF EXISTS (
+    SELECT 1 
+    FROM [Users].tblUsers 
+    WHERE email = @email AND [password] = @newPassword
+  )
+  BEGIN
+    SELECT 
+      -2 AS code,
+      'La nueva contraseña no puede ser igual a la anterior' AS message;
+    RETURN;
+  END
+
+  -- 3. Actualizar y retornar el usuario actualizado
+  UPDATE [Users].tblUsers
+  SET [password] = @newPassword
+  OUTPUT 
+    1 AS code,
+    'Contraseña actualizada correctamente' AS message,
+    INSERTED.idUser,
+    INSERTED.email
+  WHERE email = @email;
 END;
 GO
 
-EXEC [Users].splUpdateUserPassword @email = "royer.b@mail.com", @newPassword = 'passRo';
+EXEC [Users].splUpdateUserPassword @email = "royer.ba@mail.com", @newPassword = 'passRoye';
 GO
 
 SELECT * FROM [Users].tblUsers;
+
+--------------------------------------------------------
+
+DROP PROCEDURE IF EXISTS [Users].splOutputTest;
+GO
+
+CREATE PROCEDURE [Users].splOutputTest
+  @idRole INT,
+  @idUser INT
+AS
+BEGIN
+  SET NOCOUNT ON;
+
+  DECLARE @idRoleObtenido INT;
+
+  UPDATE [Users].tblRoles SET [name] = 'Admin' 
+  OUTPUT 
+    INSERTED.idRole,
+    INSERTED.name
+  WHERE idRole = @idRole;
+--  SET @idRoleObtenido = INSERTED.idRole;
+
+  UPDATE [Users].tblUsers SET [fullName] = 'Ana López' 
+  OUTPUT 
+    INSERTED.idUser,
+    INSERTED.fullName
+  WHERE idUser = @idUser;
+END;
+
+EXEC [Users].splOutputTest @idRole = 1, @idUser = 1;
+
+select * from Users.tblRoles
+select * from Users.tblUsers
+
+UPDATE [Users].tblUsers SET [fullName] = 'Miguel De Cervantes'  WHERE idUser = 1007;
+select * from Users.tblUsers
+
+      SELECT 
+        u.idUser 
+      FROM [Users].tblUsers u 
+      WHERE u.idUser = 8;
